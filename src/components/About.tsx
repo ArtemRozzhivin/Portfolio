e4c4d842f2ba3dd65a80887df14bc818a25baf2d
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2023-02-05 16:04:07 +0200

    adaptive about me block

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <div className="flex items-center">
-      <div className="flex-1/3">
+    <div className="flex items-center relative">
+      <div className="absolute h-[300px] w-[300px] xs:top-[20%] xs:-left-7 sm:top-[5%] left-0 -z-10 opacity-20 flex-1/3 md:static md:opacity-100 md:h-auto md:w-auto ">
         <img src="assets/contact.png" alt="Contact" />
       </div>
       <div className="text-right md:text-left">
         <h2 className="text-orange text-5xl mb-4">About me</h2>
         <div>
           <p className="mb-4">
-            Hello everybody! My name is Artem, I am an ambitious front-end developer from Ukraine.
-            Every day I learn something new, improve my skills in practice and theory. I strive to
-            apply my experience and skills to help businesses and companies achieve their goals by
-            creating user-centric digital products.
+            Hello everybody! My name is Artem, I am an ambitious
+            <span className="text-orange"> front-end</span> developer from Ukraine. Every day I
+            learn something new, improve my skills in practice and theory. I strive to apply my
+            experience and skills to help businesses and companies achieve their goals by creating
+            user-centric digital products.
           </p>
           <p>
             In my free time from the code, I spend it with my family, friends, watching series or
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,21 +13,22 @@ const skills = [
   { title: 'GIT', img: 'assets/git.png' },
 ];
 
-
 const Skills: React.FC = () => {
   return (
     <div className="flex items-center gap-10 relative">
-      <div className="absolute h-[300px] w-[300px] top-[10%] left-0 -z-10 opacity-20 flex-1/3 md:static md:opacity-100 md:h-auto md:w-auto">
-        <img src="assets/skills.png" alt="Skills" />
-      </div>
-      <div className='flex-auto'>
-        <h2 className="text-orange text-5xl mb-4 text-right md:text-left">My Skills</h2>
-        <div className="mb-4 text-right md:text-left">These are technologies and tools that I use when developing projects</div>
+      <div className="flex-auto">
+        <h2 className="text-orange text-5xl mb-4">My Skills</h2>
+        <div className="mb-4">
+          These are technologies and tools that I use when developing projects
+        </div>
         <div className="grid grid-cols-[repeat(auto-fit,minmax(8rem,1fr))] gap-3">
           {skills.map((obj) => (
             <SkillItem key={obj.title} title={obj.title} img={obj.img} />
           ))}
         </div>
+      </div>
+      <div className="absolute h-[300px] w-[300px] top-[10%] right-0 -z-10 opacity-20 flex-1/3 md:static md:opacity-100 md:h-auto md:w-auto">
+        <img src="assets/skills.png" alt="Skills" />
       </div>
     </div>
   );
